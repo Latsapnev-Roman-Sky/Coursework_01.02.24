3bb4b4bb9401cddc7ee1e63717979bc5cf00047c
latsa <latsapnev01@mail.ru>
2024-02-01 02:03:30 +0300

    Создал внутри класса методом "main" поле типа "Employee[10]", которое выполняет роль "хранилища" для записей сотрудников. Заполнил данные о сотрудниках.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+            Employee[] employees = new Employee[10];
+
+            employees[0] = new Employee("Роман","Лацапнев","Станиславович",1,65000);
+            employees[1] = new Employee("Евгений", "Лацапнев","Станиславович",1,60000);
+            employees[2] = new Employee("Ярослав","Фролов","Григорьевич",2,45500);
+            employees[3] = new Employee("Иван","Елисеев","Владимироваич",2,56850);
+            employees[4] = new Employee("Михаил", "Горшков","Егорович",3,59000);
+            employees[5] = new Employee("Иван","Фадеев","Иванович",3,79900);
+            employees[6] = new Employee("Василий","Богомолов","Савельевич",4,58457);
+            employees[7] = new Employee("Злата","Васильева","Викторовна",4,69921);
+            employees[8] = new Employee("Марк","Алексеев","Ярославович",5,91000);
+            employees[9] = new Employee("Полина","Березина","Данииловна",5,127883);
     }
 }
