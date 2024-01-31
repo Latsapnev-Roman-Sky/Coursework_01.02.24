c010ba92d1108763ed7118b4a5812996313a6f8f
latsa <latsapnev01@mail.ru>
2024-02-01 01:54:44 +0300

    Создал класс "Employee", который содержит информацию о ФИО, отделе и зарплате сотрудника. Добавил статическую переменную счетчик "counter", которая отвечает за "id".

---
+++ src/Employee.java
@@ -0,0 +1,9 @@
+public class Employee {
+    private String firstName;
+    private String secondName;
+    private String lastName;
+    private int department;
+    private int salary;
+    private int id;
+    private static int counter =1;
+}
